8ea31d766886367bf02922d4d897fea4745a9121
Avishek Khadka <42814492+Askay07@users.noreply.github.com>
2020-03-02 11:26:18 -0600

    Changed function

--- calculate.js
+++ calculate.js
@@ -3,7 +3,9 @@
   function calculate() {
     
     var celcius = document.getElementById("uno").value;
-    
+    TempKelvin = celcius + 273.15
+
+
     if (!isNaN(TempKelvin)) {
   
       document.getElementById("cinco").innerHTML = "$ " + TempKelvin;
